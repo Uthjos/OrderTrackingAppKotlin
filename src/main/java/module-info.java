3ed8878fc049169302cbe1b01f8e7c303e1915ce
RockyXiong47 <rockxiong83@gmail.com>
2025-11-19 11:08:52 -0600

    xml parser dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module org.metrostate.ics.ordertrackingappkotlin {
     requires javafx.controls;
     requires javafx.fxml;
     requires kotlin.stdlib;
+    requires org.json;
+    requires java.xml;
 
 
     opens org.metrostate.ics.ordertrackingappkotlin to javafx.fxml;
