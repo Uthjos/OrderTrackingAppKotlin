1fb506d29622d7b6748de83c9bd31cc9e4c4d58d
RockyXiong47 <rockxiong83@gmail.com>
2025-12-04 00:09:47 -0600

    added the delete files in Directory method in Directory rather than in the save stateOnExit

--- src/main/kotlin/org/metrostate/ics/ordertrackingappkotlin/Directory.kt
+++ src/main/kotlin/org/metrostate/ics/ordertrackingappkotlin/Directory.kt
@@ -1,7 +1,10 @@
 package org.metrostate.ics.ordertrackingappkotlin
 
 import java.io.File
+import java.io.IOException
+import java.nio.file.Files
 import java.nio.file.Paths
+import java.nio.file.StandardCopyOption
 
 /**
  * The Directory Enum holds all data directories
@@ -43,5 +46,52 @@ enum class Directory(
             createDirectory(dirPath.toString())
             return dirPath.toString()
         }
+
+
+        /**
+         * will Delete all files in the directory in argument
+         */
+        fun deleteFilesInDirectory(directory: String) {
+            var fileDir: File = File(directory)
+            var files: Array<File>? = fileDir.listFiles()
+
+            if (files != null) {
+                for (f in files) {
+                    val dest: File = File(fileDir, f.getName())
+                    try {
+                        // copy (overwrite if exists)
+                        Files.copy(f.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING)
+                    } catch (e: IOException) {
+                        System.err.println("Failed to copy " + f.getAbsolutePath() + " to " + dest.getAbsolutePath() + ": " + e.message)
+                        continue
+                    }
+
+                    // try deleting original up to 4 times with longer delay each time
+                    var deleted = false
+                    for (attempt in 1..4) {
+                        try {
+                            deleted = Files.deleteIfExists(f.toPath())
+                            if (deleted || !f.exists()) break
+                        } catch (ioe: IOException) {
+                            // ignore and retry
+                        }
+                        try {
+                            Thread.sleep((100 * attempt).toLong())
+                        } catch (ie: InterruptedException) {
+                            Thread.currentThread().interrupt()
+                            break
+                        }
+                    }
+
+                    if (!deleted) {
+                        System.err.println("Failed to delete: " + f.getAbsolutePath())
+                    }
+                }
+            }
+        }
     }
+
+
+
 }
+
