2c3abc1ba5b5840cb63e7cb7e6944de98071bacf
Ashley Zenzola <175030924+azenzola8@users.noreply.github.com>
2025-12-07 17:20:04 -0600

    Kept commented out code

--- src/main/kotlin/org/metrostate/ics/ordertrackingappkotlin/SaveState.kt
+++ src/main/kotlin/org/metrostate/ics/ordertrackingappkotlin/SaveState.kt
@@ -12,13 +12,14 @@ class SaveState {
 //            var saveOrderPath: String = Directory.getDirectory(Directory.savedOrders)
 //            var importPath: String = Directory.getDirectory(Directory.importOrders)
 
+//            orderDriver.saveAllOrdersToJSON(historyOrdersPath)
+//            Directory.deleteFilesInDirectory(saveOrderPath)
+//            Directory.deleteFilesInDirectory(importPath)
+
             orderDriver.saveAllOrdersToJSON(Directory.getDirectory(Directory.historyOrders))
 
             Directory.deleteFilesInDirectory(Directory.savedOrders)
             Directory.deleteFilesInDirectory(Directory.importOrders)
-//            orderDriver.saveAllOrdersToJSON(historyOrdersPath)
-//            Directory.deleteFilesInDirectory(saveOrderPath)
-//            Directory.deleteFilesInDirectory(importPath)
         }
     }
 
