75180f87549ff171fe22d08adc2e7d38d286192d
RockyXiong47 <rockxiong83@gmail.com>
2025-12-08 12:46:51 -0600

    Just removed the old code

--- src/main/kotlin/org/metrostate/ics/ordertrackingappkotlin/SaveState.kt
+++ src/main/kotlin/org/metrostate/ics/ordertrackingappkotlin/SaveState.kt
@@ -10,14 +10,6 @@ class SaveState {
          * @param orderDriver
          */
         fun saveStateOnExit(orderDriver : OrderDriver){
-//            var historyOrdersPath: String = Directory.getDirectory(Directory.historyOrders)
-//            var saveOrderPath: String = Directory.getDirectory(Directory.savedOrders)
-//            var importPath: String = Directory.getDirectory(Directory.importOrders)
-
-//            orderDriver.saveAllOrdersToJSON(historyOrdersPath)
-//            Directory.deleteFilesInDirectory(saveOrderPath)
-//            Directory.deleteFilesInDirectory(importPath)
-
             orderDriver.saveAllOrdersToJSON(Directory.getDirectory(Directory.historyOrders))
 
             Directory.deleteFilesInDirectory(Directory.savedOrders)
