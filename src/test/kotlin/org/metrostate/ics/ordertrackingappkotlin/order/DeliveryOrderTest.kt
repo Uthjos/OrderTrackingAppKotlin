89d65357c25ec71b9658679b8c769271f464c0e8
Joseph Murtha <josephmurtha@gmail.com>
2025-12-08 16:12:44 -0600

    Unit testing for DeliveryOrderTest.kt

--- src/test/kotlin/org/metrostate/ics/ordertrackingappkotlin/order/DeliveryOrderTest.kt
+++ src/test/kotlin/org/metrostate/ics/ordertrackingappkotlin/order/DeliveryOrderTest.kt
@@ -5,31 +5,37 @@ import org.junit.jupiter.api.Test
 
 class DeliveryOrderTest {
     @Test
-    fun getKitchenTip() {
-    }
-
-    @Test
-    fun setKitchenTip() {
-    }
-
-    @Test
-    fun getDriverTip() {
-    }
-
-    @Test
-    fun setDriverTip() {
-    }
-
-    @Test
-    fun getTotalTips() {
-    }
-
-    @Test
     fun calculateGrandTotal() {
+        val testDeliveryOrder = DeliveryOrder()
+        testDeliveryOrder.addFoodItem(FoodItem("Apple",10,4.33))
+        testDeliveryOrder.setKitchenTip(1.00)
+        testDeliveryOrder.setDriverTip(5.00)
+
+        val expectedValue= 49.3
+        assertEquals(expectedValue, testDeliveryOrder.calculateGrandTotal())
     }
 
     @Test
     fun toStringTest() {
+        val testDeliveryOrder = DeliveryOrder()
+        testDeliveryOrder.addFoodItem(FoodItem("Apple",10,4.33))
+        testDeliveryOrder.setKitchenTip(1.00)
+        testDeliveryOrder.setDriverTip(5.00)
+
+        assertEquals("Order #0\n" +
+                "1969-12-31 18:00 CST\n" +
+                "\n" +
+                "Status: Waiting\n" +
+                "Type: Delivery\n" +
+                "Items:\n" +
+                "  10x Apple - \$4.33 each\n" +
+                "\n" +
+                "Total Price: \$43.30\n" +
+                "Kitchen Tip: \$1.00\n" +
+                "Driver Tip: \$5.00\n" +
+                "\n" +
+                "Grand Total: \$49.30",testDeliveryOrder.toString())
+
     }
 
 }
